2ef8e5dc1261becd62d660ae9d203f16eb39f70e
emaintenance <Matthieu-externe.Perrin@edf.fr>
2016-08-11 13:52:10 +0200

    update

--- central/db/sql_optimize_log.sh
+++ central/db/sql_optimize_log.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Matthieu PERRIN - 2016
+# THIS SCRIPT IS PROVIDED "AS IS", WITHOUT ANY WARRANTY, AND WITHOUT ANY SUPPORT. 
+# LICENSE : CC-BY-SA 3.0 - http://creativecommons.org/licenses/by-sa/3.0/ 
 
 pgrep mysql > /dev/null && exit 1
 
